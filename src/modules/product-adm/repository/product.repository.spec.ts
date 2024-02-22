80e55deca51f90d9b6cc43b90260306fc1db4d70
William Brito <williamrldb@gmail.com>
2024-02-22 14:56:02 -0400

    --implementing find a product

--- src/modules/product-adm/repository/product.repository.spec.ts
+++ src/modules/product-adm/repository/product.repository.spec.ts
@@ -2,6 +2,7 @@ import { Sequelize } from "sequelize-typescript";
 import { ProductModel } from "./product.model";
 import { Product } from "../domain/product.entity";
 import { ProductRepository } from "./product.repository";
+import { Id } from "../../@shared/domain/value-object/id.value-object";
 
 describe("Product repository unit test", () => {
   let sequelize: Sequelize;
@@ -12,9 +13,8 @@ describe("Product repository unit test", () => {
       storage: ":memory:",
       logging: false,
       sync: { force: true },
-      models: [ProductModel],
     });
-
+    sequelize.addModels([ProductModel]);
     await sequelize.sync();
   });
 
@@ -32,5 +32,37 @@ describe("Product repository unit test", () => {
     const product = new Product(props);
     const productRepository = new ProductRepository();
     await productRepository.create(product);
+
+    const db = await ProductModel.findOne({
+      where: { id: product.id.id },
+    });
+
+    expect(product.id.id).toBe(db.getDataValue("id"));
+    expect(product.name).toBe(db.getDataValue("name"));
+    expect(product.description).toBe(db.getDataValue("description"));
+    expect(product.purchasePrice).toBe(db.getDataValue("purchasePrice"));
+    expect(product.stock).toBe(db.getDataValue("stock"));
+  });
+
+  it("should find a product", async () => {
+    const productRepository = new ProductRepository();
+    const idToFind = new Id();
+    ProductModel.create({
+      id: idToFind.id,
+      name: "Product 1",
+      description: "Description 1",
+      purchasePrice: 100,
+      stock: 10,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+
+    const product = await productRepository.find(idToFind.id);
+
+    expect(product.id.id).toBe(idToFind.id);
+    expect(product.name).toBe("Product 1");
+    expect(product.description).toBe("Description 1");
+    expect(product.purchasePrice).toBe(100);
+    expect(product.stock).toBe(10);
   });
 });
--- src/modules/product-adm/repository/product.repository.ts
+++ src/modules/product-adm/repository/product.repository.ts
@@ -1,3 +1,4 @@
+import { Id } from "../../@shared/domain/value-object/id.value-object";
 import { Product } from "../domain/product.entity";
 import { ProductGateway } from "../gateway/product.gateway";
 import { ProductModel } from "./product.model";
@@ -10,13 +11,29 @@ class ProductRepository implements ProductGateway {
       description: product.description,
       purchasePrice: product.purchasePrice,
       stock: product.stock,
-      createdAt: product.createdAt,
-      updatedAt: product.updatedAt,
+      createdAt: new Date(),
+      updatedAt: new Date(),
     });
   }
 
   async find(id: string): Promise<Product> {
-    throw new Error("Method not implemented.");
+    const product = await ProductModel.findOne({
+      where: { id },
+    });
+
+    if (!product) {
+      throw new Error(`Product with id ${id} not found`);
+    }
+
+    return new Product({
+      id: new Id(product.getDataValue("id")),
+      name: product.getDataValue("name"),
+      description: product.getDataValue("description"),
+      purchasePrice: product.getDataValue("purchasePrice"),
+      stock: product.getDataValue("stock"),
+      createdAt: product.getDataValue("createdAt"),
+      updatedAt: product.getDataValue("updatedAt"),
+    });
   }
 }
 
